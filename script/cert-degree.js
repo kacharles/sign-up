8de487052eecf9d0833e2906bed22bb4cadc45d6
kacharles <koenigalfredcharles@gmail.com>
2025-06-13 01:43:57 +0000

    Update Sign-up page

---
+++ script/cert-degree.js
@@ -0,0 +1,38 @@
+const degrees = [
+    {
+        title: 'Software Engineering',
+        certificate: [
+            {
+              certName: '',
+              certType: '',
+              course: {
+                code: '',
+                name: '',
+                credit: ''
+              }
+
+            },
+              {
+              certName: '',
+              certType: '',
+              course: {
+                code: '',
+                name: '',
+                credit: ''
+              }
+
+            },
+              {
+              certName: '',
+              certType: '',
+              course: {
+                code: '',
+                name: '',
+                credit: ''
+              }
+
+            }
+            
+        ]
+    }
+]
